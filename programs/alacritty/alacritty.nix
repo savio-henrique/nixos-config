eef168b6f5faaf2985766a6c3465ccbec7279dea
Sávio Henrique <savio.c.mendes@gmail.com>
2024-04-25 21:09:09 -0300

    :sparkle: feat: added alacritty theme with nix-colors

--- programs/alacritty/alacritty.nix
+++ programs/alacritty/alacritty.nix
@@ -8,10 +8,34 @@
 	 		title = "Terminal";
 	 		dynamic_title = false;
 		};
-	 	colors = {
-			primary = with config.colorScheme.colors; {
-	 			foreground = "0x${base06}";
+	 	colors = with config.colorScheme.colors; {
+			bright = {
+				black = "0x${base00}";
+				blue = "0x${base0D}";
+				cyan = "0x${base0C}";
+				green = "0x${base0B}";
+				magenta = "0x${base0E}";
+				red = "0x${base08}";
+				white = "0x${base06}";
+				yellow = "0x${base09}";
+			};
+			cursor = {
+				cursor = "0x${base06}";
+				text = "0x${base06}";
+			};
+			normal = {
+				black = "0x${base00}";
+				blue = "0x${base0D}";
+				cyan = "0x${base0C}";
+				green = "0x${base0B}";
+				magenta = "0x${base0E}";
+				red = "0x${base08}";
+				white = "0x${base06}";
+				yellow = "0x${base0A}";
+			};
+			primary = {
 	 			background = "0x${base00}";
+	 			foreground = "0x${base06}";
 	 		};
 		};
   };
