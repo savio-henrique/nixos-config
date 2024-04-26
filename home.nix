d7ab7ecc53165c64a8ad09f3387af41fe96c54b5
Sávio Henrique <savio.c.mendes@gmail.com>
2024-04-26 15:23:48 -0300

    :sparkle: feat: modularize awesome theme colors with nix-colors + change picom configurations

--- home.nix
+++ home.nix
@@ -40,40 +40,49 @@
   # File links
   home.file = with config.colorScheme.colors; {
     ".config/neofetch".source = dotfiles/neofetch;
-    ".config/awesome".source = dotfiles/awesome;
-		".config/awesome/theme/colors.lua".text = ''
-			local theme = {}
+    ".config/awesome" = {
+			source = dotfiles/awesome;
+			recursive = true;
+		};
 
-			-- Theme Colors
-			theme.bg_normal     = "#${base00}"
-			theme.bg_focus      = "#${base03}"
-			theme.bg_urgent     = "#${base08}"
-			theme.bg_minimize   = "#${base02}"
-			theme.bg_systray    = theme.bg_normal
-			
-			theme.fg_normal     = "#${base06}"
-			theme.fg_focus      = "#${base07}"
-			theme.fg_urgent     = "#${base07}"
-			theme.fg_minimize   = "#${base07}"
-			
-			theme.useless_gap   = dpi(4)
-			theme.border_width  = dpi(2)
-			theme.border_normal = "#${base09}"
-			theme.border_focus  = "#${base0F}"
-			theme.border_marked = "#${base0E}"
+		".config/awesome/theme/colors.lua" = {
+			target = ".config/awesome/theme/colors.lua";
+			text = ''
+local xresources = require("beautiful.xresources")
+local dpi = xresources.apply_dpi
+local theme = {}
 
-			theme.titlebar_bg_color   = theme.border_focus
-			theme.titlebar_fg_color   = theme.border_focus
-			
-			theme.tasklist_bg_focus  = theme.border_focus
-			theme.tasklist_bg_normal = theme.border_normal
-			theme.taglist_bg_normal  = theme.border_normal
-			theme.taglist_bg_focus   = theme.border_focus
-			
-			return theme
-		'';
-  };
-
+-- Theme Colors
+theme.bg_normal     = "#${base00}"
+theme.bg_focus      = "#${base03}"
+theme.bg_urgent     = "#${base08}"
+theme.bg_minimize   = "#${base02}"
+theme.bg_systray    = theme.bg_normal
+	
+theme.fg_normal     = "#${base06}"
+theme.fg_focus      = "#${base07}"
+theme.fg_urgent     = "#${base07}"
+theme.fg_minimize   = "#${base07}"
+	
+theme.useless_gap   = dpi(4)
+theme.border_width  = dpi(2)
+theme.border_normal = "#${base0F}"
+theme.border_focus  = "#${base09}"
+theme.border_marked = "#${base0E}"
+	
+theme.titlebar_bg_color  = theme.border_focus
+theme.titlebar_fg_color  = theme.border_focus
+	
+theme.tasklist_bg_focus  = theme.border_focus
+theme.tasklist_bg_normal = theme.border_normal
+theme.taglist_bg_normal  = theme.border_normal
+theme.taglist_bg_focus   = theme.border_focus
+	
+return theme
+			'';
+  	};
+	};
+	
   # Session Variables
   home.sessionVariables = {
     EDITOR = "nvim";
