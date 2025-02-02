dfa838eefe4599533c46d15522bc78550d21370e
Sávio Henrique <savio.c.mendes@gmail.com>
2025-02-01 23:26:16 -0300

    :sparkles: feat: add nvim and cli config to saviohc home.nix

--- home/saviohc/home.nix
+++ home/saviohc/home.nix
@@ -4,10 +4,9 @@
   # Imports
   imports = [
     inputs.nix-colors.homeManagerModules.default
-    ./features/git.nix
-    ./features/fish.nix
+    ../features/cli
+    ../features/nvim
     # ./programs/alacritty/alacritty.nix
-    # ./programs/nvim/nvim.nix
     # ./programs/freecad/freecad.nix
   ];
 
@@ -117,42 +116,6 @@
   #     '';
   # 	};
   # };
-	
-  # Session Variables
-  home.sessionVariables = {
-    EDITOR = "nvim";
-  };
-
-  # Program Config
-  programs = {
-
-    # Fish config
-    fish = {
-      enable = true;
-    #   shellInit = ''
-    #     echo -e "\033[4;35m'Nenhum cidadão tem o direito de ser um amador em matéria de treinamento físico. Que desgraça é para o homem envelhecer sem nunca ver a beleza e a força do que o seu corpo é capaz.'\033[0m \033[1;35m- \033[1;95m Sócrates";
-    #   '';
-    };
-  };
-
-  # Bash alias
-  home.shellAliases = {
-    # Normal aliases
-    c = "clear";
-    v = "nvim";
-    e = "exit";
-
-    # Nixos Aliases
-    rebuild = "sudo nixos-rebuild switch --flake \'/etc/nixos#saviohc\'";
-    perms = "sudo chmod -R 775 /etc/nixos && sudo chown -R root:nixos-dev /etc/nixos";
-    flake = "nix flake update";
-    dev = "nix develop --command fish";
-
-    # Tmux Aliases
-    m = "tmux";
-    ma = "tmux attach -t";
-    mr = "tmux rename-session -t";
-  };
 
   # programs.obs-studio = {
   #   enable = true;
