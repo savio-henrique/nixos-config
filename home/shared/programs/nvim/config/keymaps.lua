9b21908690dcc7c798b70ed41a7e780a47c9adb3
Sávio Henrique <savio.c.mendes@gmail.com>
2025-02-01 23:24:51 -0300

    :wrench: fix: delete obsolete files

--- home/shared/programs/nvim/config/keymaps.lua
+++
@@ -1,5 +0,0 @@
-print("Keymaps Loaded!")
-vim.g.mapleader = ' '
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
-vim.keymap.set('n', '<leader>v', vim.cmd.vsplit)
-vim.keymap.set('n', '<leader>h', vim.cmd.split)
