be84003a3b611d1cac7788c42959378dbd75433c
Sávio Henrique <savio.c.mendes@gmail.com>
2025-05-15 00:17:15 -0300

    :sparkles: feat(Steam): Add steam feature config

---
+++ home/features/steam/default.nix
@@ -0,0 +1,9 @@
+{config, pkgs, ...}:
+{
+  programs.gamemode.enable = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+  };
+}
