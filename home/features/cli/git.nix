e7cd0d5d6b7285cc71a1618b65388dcb2bdbf865
Sávio Henrique <savio.c.mendes@gmail.com>
2025-05-15 03:55:26 -0300

    :sparkles: feat(Git): Refactor git commit script to add colors and a new third argument for scope

--- home/features/cli/git.nix
+++ home/features/cli/git.nix
@@ -3,26 +3,47 @@ let
   gc = pkgs.writeShellScriptBin "gc" ''
     # gc - git conventional commit script
 
+    # Colors
+    RED='\033[0;31m'
+    GREEN='\033[0;32m'
+    YELLOW='\033[0;33m'
+    BLUE='\033[0;34m'
+    PURPLE='\033[0;35m'
+    CYAN='\033[0;36m'
+    LIGHT_GRAY='\033[0;37m'
+    WHITE='\033[1;37m'
+    CLEAR='\033[0m'
+
     first_arg=$1;
     second_arg=$2;
+    third_arg=$3;
+    message=""
+
 
     helper_message () {
-      echo "    Git Conventional Commit Helper"
-      echo "------------------------------"
-      echo "    Usage: gc <type> <message> or gc <message>"
-      echo "    Types: feat, fix, docs, style, refactor, perf, test, chore"
-      echo "------------------------------"
-      echo "    This script helps you to create a conventional commit message."
-      echo ""
-      echo "    It takes two arguments: the type of the commit and the message."
-      echo "    Example: gc feat 'Add new feature'\n"
-      echo ""
-      echo "    If you provide only one argument, it will commit with the message only."
-      echo "    Example: gc 'Add new feature'"
-      echo ""
-
-      echo "    If you provide no arguments, it will show this help message."
-      echo "---------------------------------"
+      echo -e "    ''${BLUE}Git Conventional Commit Helper''${CLEAR}"
+      echo -e "    ''${LIGHT_GRAY}------------------------------''${CLEAR}"
+      echo -e "    ''${WHITE}Usage: ''${BLUE}gc ''${CYAN}<type> ''${WHITE}<message> ''${YELLOW}<scope>''${CLEAR}" 
+      echo -e "    or ''${BLUE}gc ''${CYAN}<type> ''${WHITE}<message>''${CLEAR}" 
+      echo -e "    or ''${BLUE}gc ''${WHITE}<message>''${CLEAR}"
+      echo -e "    ''${WHITE}Types: ''${GREEN}feat, fix, docs, style, refactor, perf, test, chore''${CLEAR}"
+      echo -e "    ''${LIGHT_GRAY}------------------------------''${CLEAR}"
+      echo -e "    This script helps you to create a conventional commit message."
+      echo -e ""
+      echo -e "    It takes one to three arguments: being, in order, the type of the commit, the message and the scope."
+      echo -e "    ''${WHITE}Usage Example: ''${CYAN}gc feat 'Add new feature' 'Feature'"
+      echo -e "    ''${WHITE}Result: ''${GREEN}':sparkles: feat(Feature): Add new feature' ''${CLEAR}"
+      echo -e ""
+      echo -e "    If you provide two arguments, it will commit the type of the commit and the message."
+      echo -e "    ''${WHITE}Usage Example: ''${CYAN}gc feat 'Add new feature'"
+      echo -e "    ''${WHITE}Result: ''${GREEN}':sparkles: feat: Add new feature' ''${CLEAR}"
+      echo -e ""
+      echo -e "    If you provide only one argument, it will commit with the message only."
+      echo -e "    ''${WHITE}Usage Example: ''${CYAN}gc 'Add new feature'"
+      echo -e "    ''${WHITE}Result: ''${GREEN}'Add new feature' ''${CLEAR}"
+      echo -e ""
+      echo -e "    If you provide no arguments, it will show this help message."
+      echo -e "    ''${LIGHT_GRAY}---------------------------------''${CLEAR}"
     }
 
     # Check if the first arg is empty
@@ -32,7 +53,7 @@ let
     else 
       # Check if the first arg is a type and the second arg is empty
       if [ "$first_arg" = "feat" ] || [ "$first_arg" = "fix" ] || [ "$first_arg" = "docs" ] || [ "$first_arg" = "style" ] || [ "$first_arg" = "refactor" ] || [ "$first_arg" = "perf" ] || [ "$first_arg" = "test" ] || [ "$first_arg" = "chore" ] && [ -z "$second_arg" ]; then
-        echo "Please provide a commit message."
+        echo -e "''${RED}Please provide a commit message."
         exit 0
       fi
     fi
@@ -46,47 +67,61 @@ let
     # Check if the first arg is a valid commit type
     case $first_arg in 
       feat)
-        git commit -m ":sparkles: $first_arg: $second_arg"
+        message=":sparkles: $first_arg"
         ;;
       fix)
-        git commit -m ":wrench: $first_arg: $second_arg"
+        message=":wrench: $first_arg"
         ;;
       docs)
-        git commit -m ":pencil: $first_arg: $second_arg"
+        message=":pencil: $first_arg"
         ;;
       style)
-        git commit -m ":paintbrush: $first_arg: $second_arg"
+        message=":paintbrush: $first_arg"
         ;;
       refactor)
-        git commit -m ":gear: $first_arg: $second_arg"
+        message=":gear: $first_arg"
         ;;
       perf)
-        git commit -m ":bar_chart: $first_arg: $second_arg"
+        message=":bar_chart: $first_arg"
         ;;
       test)
-        git commit -m ":test_tube: $first_arg: $second_arg"
+        message=":test_tube: $first_arg"
         ;;
       chore)
-        git commit -m ":broom: $first_arg: $second_arg"
+        message=":broom: $first_arg"
         ;;
       *)
         if [ -z "$second_arg" ]; then
-          echo "Your commit message is: '$first_arg'. Are you sure? ([y]es/[n]o)"
-          read answer
-          if [ "$answer" = "y" ] || [ "$answer" = "yes" ]; then
-            git commit -m "$first_arg"
-            exit 0
-          else 
-            echo "Please provide a valid message."
-            exit 0
-          fi
+          message=$first_arg
         else 
-          echo "Please provide only one message."
+          echo -e "''${RED}Wrong type. Please provide a valid type."
           exit 0
         fi
         ;;
 
     esac
+
+    if ! [ -z "$third_arg" ]; then
+      full_message="''${message}(''${third_arg}): $second_arg"
+    else
+      # Check if the second arg is empty
+      if ! [ -z "$second_arg" ]; then
+        full_message="''${message}: $second_arg"
+      else
+        full_message="$message"
+      fi
+    fi
+
+    # Validates message with the user
+    echo -e "''${WHITE}Your commit message is:\n\n''${GREEN}'$full_message' ''${WHITE}\n\nAre you sure? (''${GREEN}[y]es''${WHITE}/''${RED}[n]o''${WHITE}): ''${CLEAR}"
+    read answer
+    if [ "$answer" = "y" ] || [ "$answer" = "yes" ]; then
+      # Commits message
+      git commit -m "$full_message"
+    else 
+      echo -e "''${RED}Please try again with a valid commit message."
+      exit 0
+    fi
     '';
 in {
   home.packages = [
