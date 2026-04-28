831333cf994185d5e48efa158917a5e30df434cf
Sávio Henrique <savio.c.mendes@gmail.com>
2026-04-28 19:24:21 -0300

    :sparkles: feat(Quirrel): Add Bluetooth

--- hosts/quirrel/configuration.nix
+++ hosts/quirrel/configuration.nix
@@ -53,6 +53,9 @@
     pulse.enable = true;
   };
 
+  # Enable bluetooth
+  hardware.bluetooth.enable = true;
+
   # make pipewire realtime-capable
   security.rtkit.enable = true;
 
@@ -64,6 +67,7 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     lshw
+    overskride
   ];
 
 
