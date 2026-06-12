689d9afafdd93e41cf17d91cb15adae40685d3c8
Sávio Henrique <savio.c.mendes@gmail.com>
2026-06-12 14:42:59 -0300

    :sparkles: feat: Add fish plugins

--- home/features/cli/fish.nix
+++ home/features/cli/fish.nix
@@ -32,6 +32,24 @@ in {
     # shellInit = ''
     #   echo -e "\033[4;35m'Nenhum cidadão tem o direito de ser um amador em matéria de treinamento físico. Que desgraça é para o homem envelhecer sem nunca ver a beleza e a força do que o seu corpo é capaz.'\033[0m \033[1;35m- \033[1;95m Sócrates";
     #   '';
+
+    plugins = [
+      {
+        name = "fzf";
+        src = pkgs.fishPlugins.fzf.src;
+      }
+      
+      {
+        name = "autopair";
+        src = pkgs.fishPlugins.autopair.src;
+      }
+
+      {
+        name = "colored-man-pages";
+        src = pkgs.fishPlugins.colored-man-pages.src;
+      }
+    ];
+
     functions = {
       is_nix_shell = {
         body = ''
@@ -75,7 +93,7 @@ in {
     e = "exit";
 
     # Nixos Aliases
-    rebuild = "sudo nixos-rebuild switch --flake ./#chrono";
+    rebuild = "sudo nixos-rebuild switch --flake ./#$(hostname -s)";
     perms = "sudo chmod -R 775 /etc/nixos && sudo chown -R root:nixos-dev /etc/nixos";
     flake = "nix flake update";
     dev = "nix develop --command fish";
